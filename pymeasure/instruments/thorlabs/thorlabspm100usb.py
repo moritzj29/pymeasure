a43352330544e90308986d11ce4581099178458f
Moritz Jung <18733473+moritzj29@users.noreply.github.com>
2020-06-12 17:37:32 +0200

    new methods implemented, restructuring file according to programming manual

--- pymeasure/instruments/thorlabs/thorlabspm100usb.py
+++ pymeasure/instruments/thorlabs/thorlabspm100usb.py
@@ -26,6 +26,8 @@ import time
 import logging
 
 from pymeasure.instruments import Instrument, RangeException
+from pymeasure.instruments.validators import (strict_discrete_set,
+                                              strict_range)
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
@@ -34,61 +36,25 @@ log.addHandler(logging.NullHandler())
 class ThorlabsPM100USB(Instrument):
     """Represents Thorlabs PM100USB powermeter"""
 
-    wavelength_min = Instrument.measurement(
-        "SENS:CORR:WAV? MIN", "Get minimum wavelength, in nm")
-
-    wavelength_max = Instrument.measurement(
-        "SENS:CORR:WAV? MAX", "Get maximum wavelength, in nm")
-
-    average = Instrument.control("SENS:AVERage?", "SENS:AVERage %d",
-                                 "Averaging rate")
-
-    def set_zero(self, timeout=60):
-        """Perform zero adjustment"""
-        self.write("SENS:CORR:COLLect:ZERO:INITiate")
-        start = time.time()
-        while self.values("SENS:CORR:COLLect:ZERO:STATe?")[0] is 1:
-            if time.time() > start + timeout:
-                self.write("SENS:CORR:COLLect:ZERO:ABORt")
-                log.info(
-                    "Aborted zero adjustment for %s due to timeout."
-                    % self.sensor_name)
-            else:
-                time.sleep(0.1)
-
-    zero_offset = Instrument.measurement(
-        "SENS:CORR:COLLect:ZERO:MAGnitude?", "Zero adjustment value")
-
-    beamdiameter = Instrument.control(
-        "SENS:CORR:BEAMdiameter?", "SENS:CORR:BEAMdiameter %g",
-        "Assumed beam diameter, in mm")
-    beamdiameter_min = Instrument.measurement(
-        "SENS:CORR:BEAMdiameter? MIN", "Minimum beam diameter, in mm")
-    beamdiameter_max = Instrument.measurement(
-        "SENS:CORR:BEAMdiameter? MAX", "Maximum beam diameter, in mm")
-
     def __init__(self, adapter, **kwargs):
         super(ThorlabsPM100USB, self).__init__(
-            adapter, "ThorlabsPM100USB powermeter", write_termination='\n', read_termination='\n', **kwargs)
-        self.timeout = 3000  # where is this used??
+            adapter, "ThorlabsPM100USB powermeter", write_termination='\n',
+            read_termination='\n', **kwargs)
+        self.check_errors()
         self.sensor()
 
-    def measure_power(self, wavelength):
-        """Set wavelength in nm and get power in W
-        If wavelength is out of range it will be set to range limit"""
-        if wavelength < self.wavelength_min:
-            raise RangeException(
-                "Wavelength %.2f nm out of range: using minimum wavelength: %.2f nm"
-                % (wavelength, self.wavelength_min))
-            # explicitly setting wavelenghth, although it would be autoset
-            wavelength = self.wavelength_min
-        if wavelength > self.wavelength_max:
-            raise RangeException(
-                "Wavelength %.2f nm out of range: using maximum wavelength: %.2f nm"
-                % (wavelength, self.wavelength_max))
-            wavelength = self.wavelength_max
-        self.wavelength = wavelength
-        return self.power
+    # -------------------------------
+    # SYSTem subsystem commands
+    # -------------------------------
+
+    def check_errors(self):
+        """Check if any Errors occured
+        """
+        (code, message) = self.ask("SYSTem:ERRor?").split(',')
+        if code != 0:
+            log.warning(
+                "{}: {} (Error Code: {})".format(self.name, message, code)
+                )
 
     def sensor(self):
         "Get sensor info"
@@ -112,33 +78,113 @@ class ThorlabsPM100USB(Instrument):
             self.resp_settable, self.wavelength_settable, \
             self.tau_settable, _d128, self.temperature_sens = self._flags
 
+    # -------------------------------
+    # SENSe subsystem commands
+    # -------------------------------
+
+    # AVERage
+
+    average = Instrument.control("SENS:AVERage?", "SENS:AVERage %d",
+                                 "Averaging rate")
+
+    # CORRection
+
+    def set_zero(self, timeout=60):
+        """Perform zero adjustment.
+
+        :param timeout: abort after timeout (in s), defaults to 60
+        :type timeout: int, optional
+        """
+        self.write("SENS:CORR:COLLect:ZERO:INITiate")
+        start = time.time()
+        while self.values("SENS:CORR:COLLect:ZERO:STATe?")[0] is 1:
+            if time.time() > start + timeout:
+                self.write("SENS:CORR:COLLect:ZERO:ABORt")
+                log.info(
+                    "Aborted zero adjustment for %s due to timeout."
+                    % self.sensor_name)
+            else:
+                time.sleep(0.1)
+
+    zero_offset = Instrument.measurement(
+        "SENS:CORR:COLLect:ZERO:MAGnitude?", "Zero adjustment value")
+
+    beamdiameter = Instrument.control(
+        "SENS:CORR:BEAMdiameter?", "SENS:CORR:BEAMdiameter %g",
+        "Assumed beam diameter, in mm")
+    beamdiameter_min = Instrument.measurement(
+        "SENS:CORR:BEAMdiameter? MIN", "Minimum beam diameter, in mm")
+    beamdiameter_max = Instrument.measurement(
+        "SENS:CORR:BEAMdiameter? MAX", "Maximum beam diameter, in mm")
+
+    wavelength_min = Instrument.measurement(
+        "SENS:CORR:WAV? MIN", "Get minimum wavelength, in nm")
+
+    wavelength_max = Instrument.measurement(
+        "SENS:CORR:WAV? MAX", "Get maximum wavelength, in nm")
+
     @property
     def wavelength(self):
+        """Wavelength, in nm"""
         self.values("SENSE:CORR:WAV?")[0]
 
     @wavelength.setter
     def wavelength(self, val):
-        """Wavelength in nm; not set outside of range"""
+        wavelength = strict_range(
+            wavelength, (self.wavelength_min, self.wavelength_max))
         if self.wavelength_settable:
             self.write("SENSE:CORR:WAV %g" % val)
         else:
             raise Exception(
                 "Wavelength is not settable for %s" % self.sensor_name)
 
-    @property
-    def energy(self):
-        if self.is_energy:
-            return self.values("MEAS:ENER?")[0]
-        else:
-            raise Exception("%s is not an energy sensor" % self.sensor_name)
+    # -------------------------------
+    # INPut subsystem commands
+    # -------------------------------
+
+    # -------------------------------
+    # Measurement commands
+    # -------------------------------
 
     @property
     def power(self):
         """Power, in Watts"""
         if self.is_power:
-            return self.values("MEAS:POW?")[0]
+            return self.values("MEAS:POWer?")[0]
         else:
             raise Exception("%s is not a power sensor" % self.sensor_name)
+
+    @property
+    def current(self):
+        """DC Current, in A"""
+        if self.is_power:
+            return self.values("MEAS:CURRent?")[0]
+        else:
+            raise Exception("%s is not a power sensor" % self.sensor_name)
+
+    @property
+    def voltage(self):
+        """DC Voltage, in V"""
+        if self.is_power:
+            return self.values("MEAS:VOLTage?")[0]
+        else:
+            raise Exception("%s is not a power sensor" % self.sensor_name)
+
+    @property
+    def energy(self):
+        """Energy, in J"""
+        if self.is_energy:
+            return self.values("MEAS:ENERgy?")[0]
+        else:
+            raise Exception("%s is not an energy sensor" % self.sensor_name)
+
+    @property
+    def frequency(self):
+        """Frequency, in Hz"""
+        if self.is_energy:
+            return self.values("MEAS:FREQuency?")[0]
+        else:
+            raise Exception("%s is not an energy sensor" % self.sensor_name)
 
     @property
     def power_density(self):
@@ -149,7 +195,26 @@ class ThorlabsPM100USB(Instrument):
             raise Exception("%s is not a power sensor" % self.sensor_name)
 
     @property
+    def energy_density(self):
+        """Energy Density, in J/cm2"""
+        if self.is_energy:
+            return self.values("MEAS:EDENsity?")[0]
+        else:
+            raise Exception("%s is not a energy sensor" % self.sensor_name)
+
+    @property
+    def resistance(self):
+        """Resistance, in Ohm"""
+        if self.temperature_sens:
+            return self.values("MEAS:RESistance?")[0]
+
+    @property
     def temperature(self):
         """Temperature, in °C"""
         if self.temperature_sens:
             return self.values("MEAS:TEMPerature?")[0]
+
+    def measure_power(self, wavelength):
+        """Set wavelength in nm and get power in W"""
+        self.wavelength = wavelength
+        return self.power
